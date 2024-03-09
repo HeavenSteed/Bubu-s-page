411f69146a5815be0022322a117d93cb213df1c5
HeavenSteed <146197052+HeavenSteed@users.noreply.github.com>
2024-03-09 17:36:21 +0100

    Update froggo.js

--- froggo.js
+++ froggo.js
@@ -20,7 +20,7 @@ function makeSound(button) {
       }
 
       paragraph.innerHTML = "One of the very best sounds I make, happy Froggo in the centre of my home, well, for you it might seems the centre of a swamp... No judging here!";
-      imaggio.src = "../Froggo/starry.png";
+      imaggio.src = "./Froggo/starry.png";
       let hap = new Audio("sounds/happysound.mp3");
       currentlyPlayingAudio = hap;
       hap.play();
@@ -33,7 +33,7 @@ function makeSound(button) {
       }
 
       paragraph.innerHTML = "That's a sound of an upset Froggo! It's short but shows that I'm at my limit. Don't make things worse in this case, would you? Pretty please?";
-      imaggio.src = "../Froggo/bigcry.png";
+      imaggio.src = "./Froggo/bigcry.png";
 
       let sad = new Audio("sounds/cry.mp3");
       sad.play();
@@ -47,7 +47,7 @@ function makeSound(button) {
       }
 
       paragraph.innerHTML = "Ah, the sound of an active searching for a partner. Very alluring, don't you think? I'm an irresistable frog after all.";
-      imaggio.src = "../Froggo/lovely.png";
+      imaggio.src = "./Froggo/lovely.png";
 
       let seduce = new Audio("sounds/seducive.mp3");
       seduce.play();
@@ -61,7 +61,7 @@ function makeSound(button) {
       }
 
       paragraph.innerHTML = "When I don't undestand what's going on, or showing concern, you may say I make this sound. It sounds like I'm worrying, isn't it?";
-      imaggio.src = "../Froggo/dontunderstand.png";
+      imaggio.src = "./Froggo/dontunderstand.png";
 
       let cur = new Audio("sounds/curiousfrog.mp3");
       cur.play();
@@ -75,7 +75,7 @@ function makeSound(button) {
       }
 
       paragraph.innerHTML = "Just listen to it, it's like I'm having a dinner with sophisticated glasses and making Cin-Cin with a certain someone! We're the frogs of culture, didn't you know?";
-      imaggio.src = "../Froggo/beeee.png";
+      imaggio.src = "./Froggo/beeee.png";
 
       let sssip = new Audio("sounds/sipsound.mp3");
       sssip.play();
@@ -89,11 +89,11 @@ function makeSound(button) {
       }
 
       paragraph.innerHTML = "Let's see what sound I make when I'm very suspicious of someone. Surely never happens, me, suspect somebody? Impossible, but let's hear it out.";
-      imaggio.src = "../Froggo/nonono.png";
+      imaggio.src = "./Froggo/nonono.png";
       clearTimeout(susTimeout);
       susTimeout = setTimeout(() => {
         paragraph.innerHTML = "What the hell?! That's some sort of a very sus frog playing outta there. Hope you know what kind of a frog it is and never meet it.";
-        imaggio.src = "../Froggo/donotdoitpls.png";
+        imaggio.src = "./Froggo/donotdoitpls.png";
       }, 5000);
 
       let sussy = new Audio("sounds/sussound.mp3");
